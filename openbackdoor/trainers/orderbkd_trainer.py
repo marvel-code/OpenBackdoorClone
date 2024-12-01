0972974952998303925e35e865bfc8ce8081fe87
Rodion Kostin <rodion.kostin.999@gmail.com>
2024-12-01 16:30:19 +0300

    upd

--- openbackdoor/trainers/orderbkd_trainer.py
+++ openbackdoor/trainers/orderbkd_trainer.py
@@ -12,7 +12,6 @@ from typing import *
 class OrderBkdTrainer(Trainer):
     def __init__(
         self, 
-        epochs=3,
         **kwargs
     ):
-        super().__init__(**kwargs)
+        super().__init__(epochs=3, **kwargs)
